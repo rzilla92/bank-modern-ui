ff02155d80ce031c946524b5f5a7382f63eb9c83
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-09-15 16:55:11 +0700

    Config Tailwind & components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import { close, logo, menu} from "../assets"
+import { navlinks } from "../constants"
+
+const Navbar = () => {
+  return (
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      
+    </nav>
+  )
+}
+
+export default Navbar
